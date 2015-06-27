715c7a40916b1f07dd6c2ba40782ecb1e0cd2716
nobuoka <nobuoka@vividcode.info>
2015-06-27 13:45:45 +0900

    Apache License, Version 2.0

--- README.markdown
+++ README.markdown
@@ -7,3 +7,7 @@ This library provides useful fuculties to be used on developing Windows store ap
 
 * Windows 8.1
 * Windows Phone 8.1
+
+## License
+
+This library is released under the [Apache License, Version 2.0](http://www.apache.org/licenses/LICENSE-2.0.html).
--- WinRSJS.Test/js/default.ts
+++ WinRSJS.Test/js/default.ts
@@ -3,6 +3,22 @@
 /// <reference path="..\ts-definitions\winjs.d.ts" />
 /// <reference path="..\ts-definitions\mocha.d.ts" />
 
+/*
+Copyright 2015 Yu Nobuoka
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 // For an introduction to the Blank template, see the following documentation:
 // http://go.microsoft.com/fwlink/?LinkId=232509
 (function () {
